3a49ad17e3718ad7d1737dd3347757d0f55c15af
Gianluca Ferri <glferri@gmail.com>
2015-08-14 19:44:34 +0200

    created working code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The module defines a matrix object in which its inverse is cached using R lexycal scoping.
 
-## Write a short comment describing this function
+## Create a cached object passing matrix x. The inverese matrix value is intially set to NULL. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        
+        get <- function() x
+        
+        setinv <- function(invmat) inv <<- invmat
+        
+        getinv <- function() inv
+        
+        list(set = set, get = get,
+             setinv = setinv, getinv = getinv)
+        
 }
 
 
-## Write a short comment describing this function
+## Returns the inverse of the given CacheMatrix. If the object already contains the inverse
+## no calculation is done and the cached value is returned.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
 }
